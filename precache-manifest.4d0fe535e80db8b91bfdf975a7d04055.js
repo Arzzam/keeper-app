464269b1994fde815ad4ed5cc6d3a78ffad9f44a
Arzzam <92790024+Arzzam@users.noreply.github.com>
2022-01-30 15:49:32 +0530

    Updates

---
+++ precache-manifest.4d0fe535e80db8b91bfdf975a7d04055.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "132c4f5f5d24249f2e30fe743bfbcf6c",
+    "url": "/keeper-app/index.html"
+  },
+  {
+    "revision": "cf609c5b1eda5e6ed70a",
+    "url": "/keeper-app/static/js/2.4b2afaee.chunk.js"
+  },
+  {
+    "revision": "8275250a1b29ecac9202",
+    "url": "/keeper-app/static/js/main.9da71d81.chunk.js"
+  },
+  {
+    "revision": "7491953a57bd06d42130",
+    "url": "/keeper-app/static/js/runtime~main.afea614e.js"
+  }
+]);
