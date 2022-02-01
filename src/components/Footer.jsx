b1be1a1a39930334a073d22921c2a84bc85111dd
Nafil Arzzam <92790024+Arzzam@users.noreply.github.com>
2022-02-01 18:25:18 +0530

    Name Updated

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,10 +2,12 @@ import React from "react";
 
 const cr = "Copyright ⓒ ";
 const year = new Date().getFullYear();
+const name = "Made By Nafil Arzzam";
 
 function Footer() {
   return (
     <footer>
+    <p> {name} </p>
       <p>
         {cr}
         {year}
